93320bf82bac572ec67dbcbd63831386abbd1a8b
Rodrigo Martins <rodrigo.franca.martins@rabobank.nl>
2024-12-03 13:50:13 +0000

    refactor 1

---
+++ src/main/java/trivia/enums/Category.java
@@ -0,0 +1,14 @@
+package trivia.enums;
+
+@Getter
+@RequiredArgsConstructor
+public enum Category {
+    POP("Pop"),
+    ROCK("Rock"),
+    SCIENCE("Science"),
+    SPORTS("Sports");
+
+    private final String status;
+}
+
+
